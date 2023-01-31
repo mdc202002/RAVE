084b114ac471bafc8f29cbd578466dd349034215
mdc202002 <87825591+mdc202002@users.noreply.github.com>
2023-01-30 23:41:38 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setuptools.setup(
         "rave = scripts.main_cli:main",
     ]},
     install_requires=requirements.split("\n"),
-    python_requires='>=3.8.10',
+    python_requires='>=3.10',
     include_package_data=True,
 )
